8b622ca1d993ba6d6e6ad52142a238addf41d6f0
Shlomi <shlomikaufman91@gmail.com>
2026-08-23 12:20:57 +0300

    feat: add create new game deatail

---
+++ backend/src/utils/helper.js
@@ -0,0 +1,30 @@
+import { loadJson } from "./data.js";
+import path from "path";
+
+const filePath = path.join(process.cwd(), "../../", "map.js");
+async function getTerritories() {
+  const territories = await loadJson(filePath);
+
+  for (const territorie of territories) {
+    territorie.soldiers = territorie.headquarters ? 8 : 4;
+    territorie.owner = territorie.startOwner;
+  }
+  return territories;
+}
+
+async function createGame(name) {
+  const territories = await getTerritories();
+
+  const player = {
+    name,
+    round: 1,
+    phase: "reinforce",
+    status: "playing",
+    winner: null,
+    territories: territories.filter((terr) => terr.owner === "player"),
+  };
+
+  return player;
+}
+
+console.log(await createGame("Momo"));
