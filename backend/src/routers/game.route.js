74d4d06e0bf5d97d42a879c1bfc3b4982379277c
Shlomi <shlomikaufman91@gmail.com>
2026-08-23 17:30:06 +0300

    feat: add create new game at route

---
+++ backend/src/routers/game.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+export default function createGameRouter(gameService) {
+  const router = express.Router();
+
+  router.post("/games", async (req, res) => {
+    const playerName = req.body;
+    const game = await gameService.createNewGame({ playerName });
+
+    res.status(201).send(game);
+  });
+}
