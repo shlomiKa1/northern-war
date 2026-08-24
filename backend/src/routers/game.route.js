58ef527db8d1e6bdf77b02354503375cef362553
Shlomi <shlomikaufman91@gmail.com>
2026-08-24 09:45:29 +0300

    Feat: add get game by id

--- backend/src/routers/game.route.js
+++ backend/src/routers/game.route.js
@@ -3,7 +3,15 @@ import express from "express";
 export default function createGameRouter(gameService) {
   const router = express.Router();
 
-  router.post("/games", async (req, res) => {
+  router.get("/games/:id", async (req, res) => {
+    const id = req.params.id;    
+
+    const game = await gameService.getGame(id);
+
+    res.send(game);
+  });
+
+  router.post("/", async (req, res) => {
     const playerName = req.body;
     const game = await gameService.createNewGame(playerName);
 
--- backend/src/services/game.service.js
+++ backend/src/services/game.service.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { schemaNewGame } from "../modules/game.js";
 import { createGame } from "../utils/helper.js";
 
@@ -7,21 +8,30 @@ function errMessage(message, statusCode) {
 }
 
 export default function createeGameService(gameRepo) {
-    async function createNewGame(playerName) {
-      const parsed = schemaNewGame.safeParse(playerName);
+  async function createNewGame(playerName) {
+    const parsed = schemaNewGame.safeParse(playerName);
 
-    if (!parsed.success ) {
-      throw new errMessage(parsed.error.issues, 400 );
+    if (!parsed.success) {
+      throw new errMessage(parsed.error.issues, 400);
     }
 
-    
     const game = await createGame(parsed.data);
     const create = await gameRepo.create(game);
 
     game.id = create.insertedId;
+    delete game._id;
 
     return { game, playerEvent: null, computerEvents: [] };
   }
 
-  return { createNewGame };
+  async function getGame(id) {    
+    const game = await gameRepo.findOne({_id: new ObjectId(id)});
+
+    if (!game) {
+      throw new errMessage("Game Not Found", 404);
+    }
+    return game;
+  }
+
+  return { createNewGame, getGame };
 }
