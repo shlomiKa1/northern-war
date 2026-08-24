75e0a25bc19d8693c629a30b1cb2154e71bca382
Shlomi <shlomikaufman91@gmail.com>
2026-08-24 09:21:06 +0300

    chore: start game new at service

---
+++ backend/src/services/game.service.js
@@ -0,0 +1,28 @@
+import { schemaNewGame } from "../modules/game.js";
+import { createGame } from "../utils/helper.js";
+
+function errMessage(message, statusCode) {
+  const objMessage = typeof message === "object" ? message.error : message;
+  return { message, statusCode };
+}
+
+export default function createeGameService(gameRepo) {
+    async function createNewGame(playerName) {
+      const parsed = schemaNewGame.safeParse(playerName);
+
+    if (!parsed.success ) {
+    //   throw new Error({ message: parsed.error.issues, status: 400 });
+      throw new errMessage(parsed.error.issues, 400 );
+    }
+
+    
+    const game = await createGame(parsed.data);
+    const create = await gameRepo.create(game);
+
+    game.id = create.insertedId;
+
+    return { game, playerEvent: null, computerEvents: [] };
+  }
+
+  return { createNewGame };
+}
