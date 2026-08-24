c359924e08a12ef8d845a07a8301f089bd1c1c3b
Shlomi <shlomikaufman91@gmail.com>
2026-08-24 10:43:33 +0300

    Feat: add route for reinforce

--- backend/src/routers/game.route.js
+++ backend/src/routers/game.route.js
@@ -3,8 +3,8 @@ import express from "express";
 export default function createGameRouter(gameService) {
   const router = express.Router();
 
-  router.get("/games/:id", async (req, res) => {
-    const id = req.params.id;    
+  router.get("/:id", async (req, res) => {
+    const id = req.params.id;
 
     const game = await gameService.getGame(id);
 
@@ -16,6 +16,14 @@ export default function createGameRouter(gameService) {
     const game = await gameService.createNewGame(playerName);
 
     res.status(201).send(game);
+  });
+
+  router.post("/:id/reinforce", async (req, res) => {
+    const gameId = req.params.id;
+    const territorieId = req.body;
+    const game = await gameService.reinforce(territorieId, gameId);
+
+    res.send(game);
   });
 
   return router;
