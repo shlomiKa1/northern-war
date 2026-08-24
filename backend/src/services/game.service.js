59cd3c329c9f61ab66286e0dad2d975223910f91
Shlomi <shlomikaufman91@gmail.com>
2026-08-24 10:42:21 +0300

    feat: add reinforce at service

--- backend/src/services/game.service.js
+++ backend/src/services/game.service.js
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
-import { schemaNewGame } from "../modules/game.js";
-import { createGame } from "../utils/helper.js";
+import { schemaNewGame, schemaReinforce } from "../modules/game.js";
+import { createGame, reinforcement } from "../utils/helper.js";
 
 function errMessage(message, statusCode) {
   const objMessage = typeof message === "object" ? message.error : message;
@@ -24,8 +24,8 @@ export default function createeGameService(gameRepo) {
     return { game, playerEvent: null, computerEvents: [] };
   }
 
-  async function getGame(id) {    
-    const game = await gameRepo.findOne({_id: new ObjectId(id)});
+  async function getGame(id) {
+    const game = await gameRepo.findOne({ _id: new ObjectId(id) });
 
     if (!game) {
       throw new errMessage("Game Not Found", 404);
@@ -33,5 +33,29 @@ export default function createeGameService(gameRepo) {
     return game;
   }
 
-  return { createNewGame, getGame };
+  async function reinforce(territoryId, playerId) {
+    const parsed = schemaReinforce.safeParse(territoryId);
+    if (!parsed.success) {
+      throw new errMessage(parsed.error.issues, 400);
+    }
+
+    const player = await getGame(playerId);
+
+    if (player.phase !== "reinforce") {
+      throw new errMessage("Phase must be 'reinforce'", 422);
+    }
+
+    territoryId = parsed.data.territoryId;
+
+    reinforcement(territoryId, player);
+
+    await gameRepo.update(playerId, player);
+    return {
+      player,
+      playerEvent: `Add three soldiers to ${territoryId}`,
+      computerEvents: [],
+    };
+  }
+
+  return { createNewGame, getGame, reinforce };
 }
