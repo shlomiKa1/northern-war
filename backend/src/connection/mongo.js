34359907cd7fb0a0c7413455b1102daa2005e36b
Shlomi <shlomikaufman91@gmail.com>
2026-08-24 09:22:36 +0300

    fix: connection to mongo

--- backend/src/connection/mongo.js
+++ backend/src/connection/mongo.js
@@ -1,13 +1,14 @@
 import { Db, MongoClient } from "mongodb";
 import { MONGO_DB, MONGO_URI } from "../config.js";
 
+const client = new MongoClient(MONGO_URI);
 let /**@type {Db} */ db;
 
 export async function connectToMongo() {
   if (!db) {
     try {
-      const client = new MongoClient(MONGO_URI);
-      db = await client.connect(MONGO_DB);
+      await client.connect();
+      db = client.db(MONGO_DB);
       console.log("Connected to mongo...");
     } catch (error) {
       console.log(error.message);
